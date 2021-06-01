4c5194b966e23049c28e8605031de4baea4c6a80
srtalaie <srt4721@yahoo.com>
2021-06-01 15:35:45 -0700

    set up schema for locations

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('./locations');
 
 const dbURI = process.env.MONGODB_URI || "mongodb://localhost/Loc8r";
 mongoose.connect(dbURI, {newUrlParser: true});
