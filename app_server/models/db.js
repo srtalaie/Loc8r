296d37483e48e2e75127498b72587c15706ff891
srtalaie <srt4721@yahoo.com>
2021-06-01 14:59:17 -0700

    Set up db connection

---
+++ app_server/models/db.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const dbURI = process.env.MONGODB_URI || "mongodb://localhost/Loc8r";
+mongoose.connect(dbURI, {newUrlParser: true});
+
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Mongoose connection error', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close( () => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    } );
+};
+
+
+//For Nodemon Restart
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+//For app termination
+process.once('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
+//For Heroku app termination
+process.once('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
+});
