81aac17ff9deaf0fadd32f54a65fc78b4012734a
srtalaie <srt4721@yahoo.com>
2021-06-10 14:47:04 -0700

    HOOKED UP LOCATIONS NEARBY ROUTE

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,7 +1,45 @@
 const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
 
-const locationsListByDistance = (req, res) => {};
+const locationsListByDistance = async(req, res) => {
+    const lng = parseFloat(req.query.lng);
+    const lat = parseFloat(req.query.lat);
+    const  near = {
+        type: "Point",
+        coordinates: [lng, lat]
+    };
+    const geoOptions = {
+        distanceField: "distance.calculated",
+        spherical: true,
+        maxDistance: 20000,
+        $limit: 10
+    };
+    try {
+        const results = await Loc.aggregate([
+            {
+                $geoNear : {
+                    near,
+                    ...geoOptions
+                }
+            }
+        ]);
+        const locations = results.map(result => {
+            return {
+                id: result._id,
+                name: result.name,
+                address: result.address,
+                rating: result.rating,
+                facilities: result.facilities,
+                distance: `${result.distance.calculated.toFixed()}m`
+            }
+        });
+        return res
+            .status(200)
+            .json(locations);
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 const locationsCreate = (req, res) => {};
 
