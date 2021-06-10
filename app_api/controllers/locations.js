97bc7758a803b11e655f85109435a350637c66e8
srtalaie <srt4721@yahoo.com>
2021-06-10 13:40:21 -0700

    find one loc route

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -5,7 +5,15 @@ const locationsListByDistance = (req, res) => {};
 
 const locationsCreate = (req, res) => {};
 
-const locationsReadOne = (req, res) => {};
+const locationsReadOne = (req, res) => {
+    Loc
+        .findById(req.params.locationid)
+        .exec((err, location) => {
+            res
+                .status(200)
+                .json(location);
+        });
+};
 
 const locationsUpdateOne = (req, res) => {};
 
