a92638a4dfdee39dc30f744e5f609c57871ddb17
srtalaie <srt4721@yahoo.com>
2021-06-10 13:43:22 -0700

    updated find one route with error handling

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -9,6 +9,17 @@ const locationsReadOne = (req, res) => {
     Loc
         .findById(req.params.locationid)
         .exec((err, location) => {
+            if (!location) {
+                return res
+                    .status(404)
+                    .json({
+                        "message": "location not found"
+                    });
+            } else if (err) {
+                return res
+                    .status(404)
+                    .json(err);
+            }
             res
                 .status(200)
                 .json(location);
