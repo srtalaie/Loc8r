d3f834164cacfbe321c5bece3da5d3fd1a1848b7
srtalaie <srt4721@yahoo.com>
2021-06-10 12:03:52 -0700

    set up basic api structure for app

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,17 @@
+const locationsListByDistance = (req, res) => {};
+
+const locationsCreate = (req, res) => {};
+
+const locationsReadOne = (req, res) => {};
+
+const locationsUpdateOne = (req, res) => {};
+
+const locationsDeleteOne = (req, res) => {};
+
+module.exports = {
+    locationsListByDistance,
+    locationsCreate,
+    locationsReadOne,
+    locationsUpdateOne,
+    locationsDeleteOne
+}
---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,14 @@
+const reviewsCreate = (req, res) => {};
+
+const reviewsReadOne = (req, res) => {};
+
+const reviewsUpdateOne = (req, res) => {};
+
+const reviewsDeleteOne = (req, res) => {};
+
+module.exports = {
+    reviewsCreate,
+    reviewsReadOne,
+    reviewsUpdateOne,
+    reviewsDeleteOne
+}
