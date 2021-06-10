35a118ac2d8b3cd8b320ad4e826e47889995899d
srtalaie <srt4721@yahoo.com>
2021-06-10 15:10:17 -0700

    create location route hooked up

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -43,7 +43,50 @@ const locationsListByDistance = async(req, res) => {
     }
 };
 
-const locationsCreate = (req, res) => {};
+const locationsCreate = (req, res) => {
+    Loc.create({
+        name: req.body.name,
+        address: req.body.address,
+        facilities: req.body.facilities.split(","),
+        coords: {
+            type: "Point",
+            coordinates: [
+                parseFloat(req.body.lng),
+                parseFloat(req.body.lat)
+            ]
+        },
+        openingTimes: [
+            {
+              days: req.body.days1,
+              opening: req.body.opening1,
+              closing: req.body.closing1,
+              closed: req.body.closed1
+            },
+            {
+              days: req.body.days2,
+              opening: req.body.opening2,
+              closing: req.body.closing2,
+              closed: req.body.closed2
+            },
+            {
+              days: req.body.days3,
+              opening: req.body.opening3,
+              closing: req.body.closing3,
+              closed: req.body.closed3
+              },
+        ]
+    }, (err, location) => {
+        if (err) {
+            res
+                .status(400)
+                .json(err);
+        } else {
+            res
+                .status(201)
+                .json(location)
+        }
+    });
+};
 
 const locationsReadOne = (req, res) => {
     Loc
