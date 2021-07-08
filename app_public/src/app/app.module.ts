2264fd1a24c53b159ca27a297d6fbdb04efe205a
srtalaie <srt4721@yahoo.com>
2021-07-08 16:12:23 -0700

    Set up dev environment for front end w/ angular

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { HomeListComponent } from './home-list/home-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeListComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeListComponent]
 })
 export class AppModule { }
---
+++ app_public/src/app/home-list/home-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-list',
+  templateUrl: './home-list.component.html',
+  styleUrls: ['./home-list.component.css']
+})
+export class HomeListComponent implements OnInit {
+
+  constructor() { }
+
+  name = 'New New Cafe';
+
+  ngOnInit(): void {
+  }
+
+}
