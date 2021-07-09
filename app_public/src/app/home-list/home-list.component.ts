25c0ec9121f95eb160f850c32438a75d32d8fa25
srtalaie <srt4721@yahoo.com>
2021-07-09 10:21:51 -0700

    set up vars for templating w/ angular/ts

--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export class Location {
+  _id!: string;
+  name!: string;
+  distance!: number;
+  address!: string;
+  rating!: number;
+  facilities!: string[];
+}
+
 @Component({
   selector: 'app-home-list',
   templateUrl: './home-list.component.html',
@@ -9,7 +19,14 @@ export class HomeListComponent implements OnInit {
 
   constructor() { }
 
-  name = 'New New Cafe';
+  location: Location = {
+    _id: '60c932cf7020fa26e01003ed',
+    name: 'New New Cafe',
+    distance: 14.0,
+    address: 'High Street, Reading',
+    rating: 3,
+    facilities: ['hot drinks', 'food', 'power']
+  }
 
   ngOnInit(): void {
   }
