de23f6c7b1d84b82c250b9b854e0c9a1cd366b03
srtalaie <srt4721@yahoo.com>
2021-07-20 16:11:03 -0700

    Build out navbar and links

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -4,17 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeListComponent } from './home-list/home-list.component';
 import { DistancePipe } from './distance.pipe';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeListComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+]
 
 @NgModule({
   declarations: [
     HomeListComponent,
-    DistancePipe
+    DistancePipe,
+    FrameworkComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
