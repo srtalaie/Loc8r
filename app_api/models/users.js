fef65efd0fb716afdf3062e0d2de3339b8d3ddbd
srtalaie <srt4721@yahoo.com>
2021-08-02 15:43:23 -0700

    added jwt

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -23,6 +24,17 @@ userSchema.methods.setPassword = function (password) {
 userSchema.methods.validPassword = function (password) {
     const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
     return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+    const expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+        exp: parseInt(expiry.getTime() / 1000, 10)
+    }, process.env.JWT_SECRET);
 };
 
 mongoose.model('User', userSchema);
